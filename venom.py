340fec9f3a1ad89ef06208225aaf0074b272e7b9
Fkingu01 <162575220+Fkingu01@users.noreply.github.com>
2024-07-05 06:07:28 +0530

    Update venom.py

--- venom.py
+++ venom.py
@@ -4,8 +4,8 @@ import logging
 from aiogram import Bot
 import asyncio
 
-API_TOKEN = '7255048090:AAH2_TjHO-6Kk8WVZiwPdbZMfSPUJhwrZNY'
-ADMIN_ID = '5588464519'
+API_TOKEN = '7199041568:AAHwsHnzYz3zPZFIHB-hWlo8lOnjT6B92wo'
+ADMIN_ID = '5736908271'
 MAX_RESTARTS = 5
 RESTART_PERIOD = 60  # Seconds
 
@@ -57,4 +57,4 @@ if __name__ == '__main__':
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        logging.info("Venom script terminated by user.")
+        logging.info("King script terminated by user.")
